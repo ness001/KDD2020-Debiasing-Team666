6540b405692b02fee872e333b8694a45d3a9ade0
ness001 <liang.li.ness@gmail.com>
2020-05-26 21:46:52 +0800

    first stage

---
+++ user_data/script.py
@@ -0,0 +1,10 @@
+now_phase=6
+for c in range(0,now_phase+1):
+    test_path = '../data/underexpose_test/'  
+    df=pd.read_csv(test_path + '/underexpose_test_click-{}/underexpose_test_click-{}.csv'.format(c,c), header=None,  names=['user_id', 'item_id', 'time'])
+#  the same outcome   
+#df.sort_values(by=['user_id','time'],ascending=False).groupby(['user_id']).agg('max').reset_index()
+    temp=df.sort_values(by=['user_id','time'],ascending=False).drop_duplicates(subset=['user_id'],keep='first')
+    temp.to_csv(test_path+'utc_click-{}.csv'.format(c))
+    df.shape,'-->',temp.shape#!/usr/bin/env python
+
